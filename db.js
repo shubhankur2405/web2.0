c803f5b14630912e0ed55ba20a65968bb736512b
shubhankur2405 <shubhankur.kumar2405@gmail.com>
2021-01-14 22:23:46 +0530

    Writing Your First Query

--- db.js
+++ db.js
@@ -1,30 +1,32 @@
-var mysql=require('mysql');
-var db_config={
-  host :'127.0.0.1',
-  user : 'root',
-  password : 'qwerty123456',
-  database :'webapp'
+var mysql = require('mysql');
+var db_config = {
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : 'golu2405golu',
+    database : 'webapp'
 }
 
-var connection ;
+var connection;
 
-funtion handleDisconnect(){
-  connection=mysql.createConnection(db_config);
-  connection.connect(function(err){
-    if(err){
-      console.log('error when connecting to db :',err);
-      setTimeout(handleDisconnect,2000);
-    }
-  });
-  connection.on('error',function(err){
-    if(err.code==='PROTOCOL_CONNECTION_LOST'){
-      handleDisconnect();
-    }else{
-      throw err;
-    }
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+    connection.connect(function(err) {
+        if(err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+
+  connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+          handleDisconnect();
+      } else {
+          throw err;
+      }
   });
 }
 
 handleDisconnect();
 
-module.exports=connection;
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express'); //to include express that we installed by "npm install express --save"
 const app =express();
 const bodyParser=require('body-parser');
+const {save_user_information} =require('./models/server_db');
 
 /*handling all the parsing*/
 app.use(bodyParser.json());
@@ -9,7 +10,7 @@ app.use(bodyParser.json());
 //   res.send("HELLO WEB 2.0")     // By this we can able tp see on the chrome browser,ie on server
 // });
 
-app.post('/',(req,res)=>{    //We are posting email and amount
+app.post('/', async (req,res)=>{    //We are posting email and amount
   var email=req.body.email;   //And we are catching them
   var amount =req.body.amount;
 
@@ -21,10 +22,10 @@ app.post('/',(req,res)=>{    //We are posting email and amount
   }
 
 
+  var result=await save_user_information({"amount" :amount,"email" : email});
 
 
-
-  res.send({'amount' : amount,'email' : email });  //and then taking the email and amount and sending it back to user
+  res.send(result);  //and then taking the email and amount and sending it back to user
  //later on it will be visible on the browser.
 });
 
